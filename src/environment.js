034bfb3e4dde9f9957fa52676ee67e045ff0a8ab
yincw <120758164@qq.com>
2017-01-12 17:24:48 +0800

    u

---
+++ src/environment.js
@@ -0,0 +1,4 @@
+// https://github.com/arasatasaygin/is.js
+// https://github.com/hotoo/detector
+// http://www.haorooms.com/post/js_pc_iosandmobile
+// https://yincw.github.io/javascript/BOM/navigator/index.html
